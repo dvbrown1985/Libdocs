d6c56fb9155148b0d009da567efbb320e9089ae7
dvbrown1985 <dnblovesg1@gmail.com>
2024-12-29 11:53:10 -0800

    Add files via upload

--- libdocs.py
+++ libdocs.py
@@ -45,6 +45,7 @@ def get_reranker():
 texts = load_and_process_docs(["founding docs.txt"])
 embeddings, index = get_embeddings_and_index(texts)
 reranker = get_reranker()
+embedder = SentenceTransformer('all-mpnet-base-v2')
 
 # --- 6. Initialize Streamlit Session State ---
 if "messages" not in st.session_state:
