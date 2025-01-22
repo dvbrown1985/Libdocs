f245d52462b3cd2df9fa3082d86aace5b1eb7878
David N. Brown <dnblovesg1@gmail.com>
2025-01-21 21:54:22 -0800

    Update libdocs.py

--- libdocs.py
+++ libdocs.py
@@ -91,7 +91,7 @@ else:
     st.write("")
 
 # --- 12. Query Handling ---
-if query := st.chat_input(" 🗽 🇺🇸 🦅 "):
+if query := st.chat_input("  🗽 🇺🇸 🦅 Input your question here 🗽 🇺🇸 🦅  "):
     
     st.session_state.messages.append({"role": "user", "content": query})
     with st.chat_message("user"):
